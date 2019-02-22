5f58c8e5b98568d49153c46d3ec6af6592edb373
ekinakyurek <akyurekekin@gmail.com>
2019-02-22 11:02:47 -0500

    fix typo in transfer learning v2

--- scripts/transfer_1000sent_full.sh
+++ scripts/transfer_1000sent_full.sh
@@ -4,24 +4,24 @@
 #SBATCH --qos=high
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --cpus-per-task=1
-#SBATCH --job-name="transfer1000"
-#SBATCH --output=transfer1000.out
-#SBATCH --error=transfer1000.error
+#SBATCH --job-name="transfer1000full"
+#SBATCH --output=transfer1000full.out
+#SBATCH --error=transfer1000full.error
 
 
 julia main.jl --lang sv --epochs 100 --trainSize 1000 --lemma \
-              --dropouts 0.5 --mode 3\
-              --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-da_size_full_epoch10.jld2
+              --dropouts 0.5 --mode 3 \
+              --sourceModel ../checkpoints/bestModel.MorseModel_lemma_true_lang_UD-da_size_full_epoch10.jld2
 
 julia main.jl --lang bg --epochs 100 --trainSize 1000 --lemma \
-              --dropouts 0.5 --mode 3\
-              --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-ru_size_full_epoch10.jld2
+              --dropouts 0.5 --mode 3 \
+              --sourceModel ../checkpoints/bestModel.MorseModel_lemma_true_lang_UD-ru_size_full_epoch10.jld2
 
 
 julia main.jl --lang hu --epochs 100 --trainSize 1000 --lemma \
-              --dropouts 0.5 --mode 3\
-              --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-fi_size_full_epoch10.jld2
+              --dropouts 0.5 --mode 3 \
+              --sourceModel ../checkpoints/bestModel.MorseModel_lemma_true_lang_UD-fi_size_full_epoch10.jld2
 
 julia main.jl --lang pt --epochs 100 --trainSize 1000 --lemma \
-            --dropouts 0.5 --mode 3\
-            --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-es_size_full_epoch10.jld2
+            --dropouts 0.5 --mode 3 \
+            --sourceModel ../checkpoints/bestModel.MorseModel_lemma_true_lang_UD-es_size_full_epoch10.jld2
--- scripts/transfer_1000sent_tag.sh
+++ scripts/transfer_1000sent_tag.sh
@@ -4,24 +4,24 @@
 #SBATCH --qos=high
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --cpus-per-task=1
-#SBATCH --job-name="transfer100"
-#SBATCH --output=transfer100.out
-#SBATCH --error=transfer100.error
+#SBATCH --job-name="transfer1000tag"
+#SBATCH --output=transfer1000tag.out
+#SBATCH --error=transfer1000tag.error
 
 
 julia main.jl --lang sv --epochs 100 --trainSize 1000  \
-              --dropouts 0.5 --mode 3\
-              --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-da_size_full_epoch10.jld2
+              --dropouts 0.5 --mode 3 \
+              --sourceModel ../checkpoints/bestModel.MorseModel_lemma_false_lang_UD-da_size_full_epoch10.jld2
 
-julia main.jl --lang bg --epochs 100 --trainSize 1000  \
-              --dropouts 0.5 --mode 3\
-              --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-ru_size_full_epoch10.jld2
+julia main.jl --lang bg --epochs 100 --trainSize 1000 \
+              --dropouts 0.5 --mode 3 \
+              --sourceModel ../checkpoints/bestModel.MorseModel_lemma_false_lang_UD-ru_size_full_epoch10.jld2
 
 
-julia main.jl --lang hu --epochs 100 --trainSize 1000  \
-              --dropouts 0.5 --mode 3\
-              --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-fi_size_full_epoch10.jld2
+julia main.jl --lang hu --epochs 100 --trainSize 1000 \
+              --dropouts 0.5 --mode 3 \
+              --sourceModel ../checkpoints/bestModel.MorseModel_lemma_false_lang_UD-fi_size_full_epoch10.jld2
 
-julia main.jl --lang pt --epochs 100 --trainSize 1000  \
-            --dropouts 0.5 --mode 3\
-            --sourceModel ../checkpoints/morse.MorseDis_lemma_true_lang_UD-es_size_full_epoch10.jld2
+julia main.jl --lang pt --epochs 100 --trainSize 1000 \
+            --dropouts 0.5 --mode 3 \
+            --sourceModel ../checkpoints/bestModel.MorseModel_lemma_false_lang_UD-es_size_full_epoch10.jld2
